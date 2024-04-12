aa14408989487eae24dc0f8877e10c4ec03499eb
Marcos Vinicius <marcosrodrigues993@hotmail.com>
2024-04-12 11:01:33 +0100

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -34,6 +37,15 @@ public class Order implements Serializable{
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	/*
+	@OneToOne
+	private Payment payment;
+	
+	*/
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Order() {
 	}
 
@@ -42,6 +54,7 @@ public class Order implements Serializable{
 		this.moment = moment;
 		setOrderStatus(orderStatus);
 		this.client = client;
+	//	this.payment = payment;
 	}
 
 	public Long getId() {
@@ -78,7 +91,23 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	/*
+	 public Long getPayment() {
 
+		return payment;
+	}
+
+	public void setPayment(Long payment) {
+		this.payment = payment;
+	}
+
+	 */
+	
+	public Set<OrderItem> getItems(){
+		return items;
+		
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
---
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -0,0 +1,84 @@
+
+/*
+
+
+package com.educandoweb.course.entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table (name = "tb_payment")
+public class Payment {
+	
+	@OneToOne
+	private Order order;
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Date moment;
+
+	public Payment(){
+	}
+
+	public Payment(Order order, Integer id, Date moment) {
+		super();
+		this.order = order;
+		this.id = id;
+		this.moment = moment;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
+
+
+*/
